578ad644768cfc1254044d1b63e50adf16862b3f
liluyang1999 <1371200859@qq.com>
2020-04-12 13:59:06 +0800

    First commit.

---
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -0,0 +1,18 @@
+package com.coolweather.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button1 = findViewById(R.id.left);
+        button1.setOnClickListener();
+    }
+}
