beded614dd3ff6ea44aaeedb9a4f28631f4517aa
liluyang1999 <1371200859@qq.com>
2020-04-12 14:18:55 +0800

    加入创建数据库和表的各项配置。

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button1 = findViewById(R.id.left);
-        button1.setOnClickListener();
+
     }
 }
